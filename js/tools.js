ee2d48da28ea8744875a7d0d6493a5aa68634115
htmltest <struev.vs@gmail.com>
2017-08-02 14:57:39 +0300

    part2

--- js/tools.js
+++ js/tools.js
@@ -1,5 +1,12 @@
 $(document).ready(function() {
 
+    $.validator.addMethod('maskPhone',
+        function(value, element) {
+            return /^\+7 \(\d{3}\) \d{3}\-\d{2}\-\d{2}$/.test(value);
+        },
+        'Не соответствует формату'
+    );
+
     $('form').each(function() {
         initForm($(this));
     });
@@ -135,20 +142,9 @@ $(document).ready(function() {
         e.preventDefault();
     });
 
-    $('.form-input input').each(function() {
-        if ($(this).val() != '') {
-            $(this).parent().addClass('focus');
-        }
-    });
-
-    $('.form-input input').focus(function() {
-        $(this).parent().addClass('focus');
-    });
-
-    $('.form-input input').blur(function() {
-        if ($(this).val() == '') {
-            $(this).parent().removeClass('focus');
-        }
+    $('body').on('click', '.window-link', function(e) {
+        windowOpen($(this).attr('href'));
+        e.preventDefault();
     });
 
 });
@@ -156,10 +152,201 @@ $(document).ready(function() {
 $(window).on('resize', function() {
     $('.form-select select').chosen('destroy');
     $('.form-select select').chosen({disable_search: true, placeholder_text_multiple: ' ', no_results_text: 'Нет результатов'});
+    $('.form-select select').each(function() {
+        var curSelect = $(this);
+        if (curSelect.data('placeholder') != '') {
+            curSelect.parent().find('.chosen-single').prepend('<strong>' + curSelect.data('placeholder') + '</strong>');
+        }
+    });
 });
 
 function initForm(curForm) {
+    curForm.find('.form-input input, .form-input textarea').each(function() {
+        if ($(this).val() != '') {
+            $(this).parent().addClass('focus');
+        }
+    });
+
+    curForm.find('.form-input input, .form-input textarea').focus(function() {
+        $(this).parent().addClass('focus');
+    });
+
+    curForm.find('.form-input input, .form-input textarea').blur(function() {
+        if ($(this).val() == '') {
+            $(this).parent().removeClass('focus');
+        }
+    });
+
+    curForm.find('input.maskPhone').mask('+7 (999) 999-99-99');
+
     curForm.find('.form-select select').chosen({disable_search: true, no_results_text: 'Нет результатов'});
+    curForm.find('.form-select select').each(function() {
+        var curSelect = $(this);
+        if (curSelect.data('placeholder') != '') {
+            curSelect.parent().find('.chosen-single').prepend('<strong>' + curSelect.data('placeholder') + '</strong>');
+        }
+    });
+
+    curForm.find('.form-file input').change(function() {
+        var curInput = $(this);
+        var curField = curInput.parent().parent().parent().parent();
+        curField.find('.form-file-name').html(curInput.val().replace(/.*(\/|\\)/, ''));
+        curField.find('label.error').remove();
+        curField.removeClass('error');
+    });
+
+    if (curForm.hasClass('window-form')) {
+        curForm.validate({
+            ignore: '',
+            invalidHandler: function(form, validatorcalc) {
+                validatorcalc.showErrors();
+                checkErrors();
+            },
+            submitHandler: function(form) {
+                windowOpen($(form).attr('action'), $(form).serialize());
+            }
+        });
+    } else {
+        curForm.validate({
+            ignore: '',
+            invalidHandler: function(form, validatorcalc) {
+                validatorcalc.showErrors();
+                checkErrors();
+            }
+        });
+    }
+}
+
+function checkErrors() {
+    $('.form-input').each(function() {
+        var curField = $(this);
+        if (curField.find('input.error').length > 0 || curField.find('textarea.error').length > 0) {
+            curField.addClass('error');
+        } else {
+            curField.removeClass('error');
+        }
+        if (curField.find('input.valid').length > 0 || curField.find('textarea.valid').length > 0) {
+            curField.addClass('valid');
+        } else {
+            curField.removeClass('valid');
+        }
+    });
+
+    $('.form-checkbox, .form-file').each(function() {
+        var curField = $(this);
+        if (curField.find('input.error').length > 0) {
+            curField.addClass('error');
+        } else {
+            curField.removeClass('error');
+        }
+        if (curField.find('input.valid').length > 0) {
+            curField.addClass('valid');
+        } else {
+            curField.removeClass('valid');
+        }
+    });
+
+    $('.form-select').each(function() {
+        var curField = $(this).parent().parent();
+        if (curField.find('select.error').length > 0) {
+            curField.addClass('error');
+        } else {
+            curField.removeClass('error');
+        }
+        if (curField.find('select.valid').length > 0) {
+            curField.addClass('valid');
+        } else {
+            curField.removeClass('valid');
+        }
+    });
+}
+
+function windowOpen(linkWindow, dataWindow) {
+    var curPadding = $('.wrapper').width();
+    $('html').addClass('window-open');
+    curPadding = $('.wrapper').width() - curPadding;
+    $('body').css({'margin-right': curPadding + 'px'});
+
+    if ($('.window').length > 0) {
+        $('.window').remove();
+    }
+
+    $('body').append('<div class="window"><div class="window-loading"></div></div>')
+
+    $.ajax({
+        type: 'POST',
+        url: linkWindow,
+        dataType: 'html',
+        data: dataWindow,
+        cache: false
+    }).done(function(html) {
+        if ($('.window').length > 0) {
+            $('.window').append('<div class="window-container window-container-load"><div class="window-content">' + html + '<a href="#" class="window-close"></a></div></div>')
+
+            if ($('.window-container img').length > 0) {
+                $('.window-container img').each(function() {
+                    $(this).attr('src', $(this).attr('src'));
+                });
+                $('.window-container').data('curImg', 0);
+                $('.window-container img').load(function() {
+                    var curImg = $('.window-container').data('curImg');
+                    curImg++;
+                    $('.window-container').data('curImg', curImg);
+                    if ($('.window-container img').length == curImg) {
+                        $('.window-container').removeClass('window-container-load');
+                        windowPosition();
+                    }
+                });
+            } else {
+                $('.window-container').removeClass('window-container-load');
+                windowPosition();
+            }
+
+            $(window).resize(function() {
+                windowPosition();
+            });
+
+            $('.window-close').click(function(e) {
+                windowClose();
+                e.preventDefault();
+            });
+
+            $('body').on('keyup', function(e) {
+                if (e.keyCode == 27) {
+                    windowClose();
+                }
+            });
+
+            $('.window form').each(function() {
+                initForm($(this));
+            });
+
+            $(document).click(function(e) {
+                if ($(e.target).hasClass('window')) {
+                    windowClose();
+                }
+            });
+        }
+    });
+}
+
+function windowPosition() {
+    if ($('.window').length > 0) {
+        $('.window-container').css({'left': '50%', 'margin-left': -$('.window-container').width() / 2});
+
+        $('.window-container').css({'top': '50%', 'margin-top': -$('.window-container').height() / 2, 'padding-bottom': 0});
+        if ($('.window-container').height() > $('.window').height() - 60) {
+            $('.window-container').css({'top': '30px', 'margin-top': 0, 'padding-bottom': 30});
+        }
+    }
+}
+
+function windowClose() {
+    if ($('.window').length > 0) {
+        $('.window').remove();
+        $('html').removeClass('window-open');
+        $('body').css({'margin-right': 0});
+    }
 }
 
 function updateCalendarMore() {
